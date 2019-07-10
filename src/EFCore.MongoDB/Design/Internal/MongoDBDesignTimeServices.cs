10c6121715e9e8edd3aa7e3a1ee84763cf0c5400
SmeeK153 <eric.freeman@ago.org>
2019-07-09 21:43:16 -0700

    Add implementation of IDesignTimeServices

---
+++ src/EFCore.MongoDB/Design/Internal/MongoDBDesignTimeServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DEF.EntityFrameworkCore.MongoDB.Design.Internal
+{
+    public class MongoDBDesignTimeServices : IDesignTimeServices
+    {
+        // TODO: .AddSingleton<ITypeMappingSource, MongoDBTypeMappingSource>()
+        // TODO: .AddSingleton<IDatabaseModelFactory, MongoDBDatabaseModelFactory>()
+        // TODO: .AddSingleton<IProviderConfigurationCodeGenerator, MongoDBCodeGenerator>()
+        // TODO: .AddSingleton<IAnnotationCodeGenerator, AnnotationCodeGenerator>();
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection) 
+            => serviceCollection;
+    }
+}
