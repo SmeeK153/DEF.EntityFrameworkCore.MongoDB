b26c253d55759063b367b3bd6b7f98c2a752c7b0
Eric <SmeeK153@users.noreply.github.com>
2019-07-23 18:59:09 -0700

    Add infrastructure resources (#4)
    * Add .gitignore
    
    * Add EFCore.MongoDB project
    
    * Add test projects
    
    * Add solution
    
    * Add NonRelational project
    
    * Add implementation of IDesignTimeServices
    
    * Add infrastructure files and some related dependencies

---
+++ src/EFCore.MongoDB/Extensions/MongoDbContextOptionsExtensions.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using EFCore.MongoDB.Infrastructure;
+using EFCore.MongoDB.Infrastructure.Internal;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+
+namespace EFCore.MongoDB.Extensions
+{
+    /// <summary>
+    ///     MongoDb-specific extension methods for <see cref="DbContextOptionsBuilder" />.
+    /// </summary>
+    public static class MongoDbContextOptionsExtensions
+    {
+        /// <summary>
+        ///     Configures the context to connect to a MongoDb database.
+        /// </summary>
+        /// <typeparam name="TContext"> The type of context to be configured. </typeparam>
+        /// <param name="optionsBuilder"> The builder being used to configure the context. </param>
+        /// <param name="accountEndpoint"> The account end-point to connect to. </param>
+        /// <param name="accountKey"> The account key. </param>
+        /// <param name="databaseName"> The database name. </param>
+        /// <param name="MongoDbOptionsAction">An optional action to allow additional MongoDb-specific configuration.</param>
+        /// <returns> The options builder so that further configuration can be chained. </returns>
+        public static DbContextOptionsBuilder<TContext> UseMongoDb<TContext>(
+            [NotNull] this DbContextOptionsBuilder<TContext> optionsBuilder,
+            [NotNull] string accountEndpoint,
+            [NotNull] string accountKey,
+            [NotNull] string databaseName,
+            [CanBeNull] Action<MongoDbContextOptionsBuilder> MongoDbOptionsAction = null)
+            where TContext : DbContext
+            => (DbContextOptionsBuilder<TContext>)UseMongoDb(
+                (DbContextOptionsBuilder)optionsBuilder,
+                accountEndpoint,
+                accountKey,
+                databaseName,
+                MongoDbOptionsAction);
+
+        /// <summary>
+        ///     Configures the context to connect to a MongoDb database.
+        /// </summary>
+        /// <param name="optionsBuilder"> The builder being used to configure the context. </param>
+        /// <param name="accountEndpoint"> The account end-point to connect to. </param>
+        /// <param name="accountKey"> The account key. </param>
+        /// <param name="databaseName"> The database name. </param>
+        /// <param name="MongoDbOptionsAction">An optional action to allow additional MongoDb-specific configuration.</param>
+        /// <returns> The options builder so that further configuration can be chained. </returns>
+        public static DbContextOptionsBuilder UseMongoDb(
+            [NotNull] this DbContextOptionsBuilder optionsBuilder,
+            [NotNull] string accountEndpoint,
+            [NotNull] string accountKey,
+            [NotNull] string databaseName,
+            [CanBeNull] Action<MongoDbContextOptionsBuilder> MongoDbOptionsAction = null)
+        {
+            Check.NotNull(optionsBuilder, nameof(optionsBuilder));
+            Check.NotNull(accountEndpoint, nameof(accountEndpoint));
+            Check.NotEmpty(accountKey, nameof(accountKey));
+            Check.NotEmpty(databaseName, nameof(databaseName));
+
+            var extension = optionsBuilder.Options.FindExtension<MongoDbOptionsExtension>()
+                            ?? new MongoDbOptionsExtension();
+
+            extension = extension
+                .WithAccountEndpoint(accountEndpoint)
+                .WithAccountKey(accountKey)
+                .WithDatabaseName(databaseName);
+
+            ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+            MongoDbOptionsAction?.Invoke(new MongoDbContextOptionsBuilder(optionsBuilder));
+
+            return optionsBuilder;
+        }
+    }
+}
---
+++ src/EFCore.MongoDB/Infrastructure/Internal/MongoDbSingletonOptions.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using EFCore.MongoDB.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+
+namespace EFCore.MongoDB.Infrastructure.Internal
+{
+    /// <summary>
+    ///     <para>
+    ///         This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///         the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///         any release. You should only use it directly in your code with extreme caution and knowing that
+    ///         doing so can result in application failures when updating to a new Entity Framework Core release.
+    ///     </para>
+    ///     <para>
+    ///         The service lifetime is <see cref="ServiceLifetime.Singleton"/>. This means a single instance
+    ///         is used by many <see cref="DbContext"/> instances. The implementation must be thread-safe.
+    ///         This service cannot depend on services registered as <see cref="ServiceLifetime.Scoped"/>.
+    ///     </para>
+    /// </summary>
+    public class MongoDbSingletonOptions : IMongoDbSingletonOptions
+    {
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual string AccountEndpoint { get; private set; }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual string AccountKey { get; private set; }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual string Region { get; private set; }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual void Initialize(IDbContextOptions options)
+        {
+            var MongoDbOptions = options.FindExtension<MongoDbOptionsExtension>();
+            if (MongoDbOptions != null)
+            {
+                AccountEndpoint = MongoDbOptions.AccountEndpoint;
+                AccountKey = MongoDbOptions.AccountKey;
+                Region = MongoDbOptions.Region;
+            }
+        }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual void Validate(IDbContextOptions options)
+        {
+            var MongoDbOptions = options.FindExtension<MongoDbOptionsExtension>();
+
+            if (MongoDbOptions != null
+                && (AccountEndpoint != MongoDbOptions.AccountEndpoint
+                    || AccountKey != MongoDbOptions.AccountKey
+                    || Region != MongoDbOptions.Region))
+            {
+                throw new InvalidOperationException(
+                    CoreStrings.SingletonOptionChanged(
+                        nameof(MongoDbContextOptionsExtensions.UseMongoDb),
+                        nameof(DbContextOptionsBuilder.UseInternalServiceProvider)));
+            }
+        }
+    }
+}
