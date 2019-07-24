140979face600f462004bb81bc0f4ed81d40b4a7
SmeeK153 <eric.freeman@ago.org>
2019-07-23 18:55:38 -0700

    Add infrastructure files and some related dependencies

---
+++ src/EFCore.MongoDB/Extensions/MongoDbServiceCollectionExtensions.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCore.MongoDB.Extensions
+{
+    /// <summary>
+    ///     MongoDb-specific extension methods for <see cref="IServiceCollection" />.
+    /// </summary>
+    public static class MongoDbServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     <para>
+        ///         Adds the services required by the MongoDb database provider for Entity Framework
+        ///         to an <see cref="IServiceCollection" />.
+        ///     </para>
+        ///     <para>
+        ///         Calling this method is no longer necessary when building most applications, including those that
+        ///         use dependency injection in ASP.NET or elsewhere.
+        ///         It is only needed when building the internal service provider for use with
+        ///         the <see cref="DbContextOptionsBuilder.UseInternalServiceProvider"/> method.
+        ///         This is not recommend other than for some advanced scenarios.
+        ///     </para>
+        /// </summary>
+        /// <param name="serviceCollection"> The <see cref="IServiceCollection" /> to add services to. </param>
+        /// <returns>
+        ///     The same service collection so that multiple calls can be chained.
+        /// </returns>
+        public static IServiceCollection AddEntityFrameworkMongoDb([NotNull] this IServiceCollection serviceCollection)
+        {
+            Check.NotNull(serviceCollection, nameof(serviceCollection));
+
+            var builder = new EntityFrameworkServicesBuilder(serviceCollection)
+                .TryAdd<LoggingDefinitions, MongoDbLoggingDefinitions>()
+                .TryAdd<IDatabaseProvider, DatabaseProvider<MongoDbOptionsExtension>>()
+                .TryAdd<IDatabase, MongoDbDatabaseWrapper>()
+                .TryAdd<IExecutionStrategyFactory, MongoDbExecutionStrategyFactory>()
+                .TryAdd<IDbContextTransactionManager, MongoDbTransactionManager>()
+                .TryAdd<IModelValidator, MongoDbModelValidator>()
+                .TryAdd<IProviderConventionSetBuilder, MongoDbConventionSetBuilder>()
+                .TryAdd<IDatabaseCreator, MongoDbDatabaseCreator>()
+                .TryAdd<IQueryContextFactory, MongoDbQueryContextFactory>()
+                .TryAdd<ITypeMappingSource, MongoDbTypeMappingSource>()
+
+                // New Query pipeline
+                .TryAdd<IQueryableMethodTranslatingExpressionVisitorFactory, MongoDbQueryableMethodTranslatingExpressionVisitorFactory>()
+                .TryAdd<IShapedQueryCompilingExpressionVisitorFactory, MongoDbShapedQueryCompilingExpressionVisitorFactory>()
+
+                .TryAdd<ISingletonOptions, IMongoDbSingletonOptions>(p => p.GetService<IMongoDbSingletonOptions>())
+                .TryAddProviderSpecificServices(
+                    b => b
+                        .TryAddSingleton<IMongoDbSingletonOptions, MongoDbSingletonOptions>()
+                        .TryAddSingleton<SingletonMongoDbClientWrapper, SingletonMongoDbClientWrapper>()
+                        //.TryAddSingleton<ISqlExpressionFactory, SqlExpressionFactory>()                   // Not sure yet why this is needed in a non-relational provider, leaving in just in-case
+                        //.TryAddSingleton<IQuerySqlGeneratorFactory, QuerySqlGeneratorFactory>()
+                        .TryAddSingleton<IMethodCallTranslatorProvider, MongoDbMethodCallTranslatorProvider>()
+                        .TryAddSingleton<IMemberTranslatorProvider, MongoDbMemberTranslatorProvider>()
+                        .TryAddScoped<MongoDbClientWrapper, MongoDbClientWrapper>()
+                );
+
+            builder.TryAddCoreServices();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ src/EFCore.MongoDB/Infrastructure/MongoDbContextOptionsBuilder.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using EFCore.MongoDB.Infrastructure.Internal;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.ComponentModel;
+
+namespace EFCore.MongoDB.Infrastructure
+{
+    /// <summary>
+    ///     <para>
+    ///         Allows MongoDb specific configuration to be performed on <see cref="DbContextOptions" />.
+    ///     </para>
+    ///     <para>
+    ///         Instances of this class are returned from a call to
+    ///         <see cref="MongoDbContextOptionsExtensions.UseMongoDb{TContext}" />
+    ///         and it is not designed to be directly constructed in your application code.
+    ///     </para>
+    /// </summary>
+    public class MongoDbContextOptionsBuilder
+    {
+        private readonly DbContextOptionsBuilder _optionsBuilder;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MongoDbContextOptionsBuilder" /> class.
+        /// </summary>
+        /// <param name="optionsBuilder"> The options builder. </param>
+        public MongoDbContextOptionsBuilder([NotNull] DbContextOptionsBuilder optionsBuilder)
+        {
+            Check.NotNull(optionsBuilder, nameof(optionsBuilder));
+
+            _optionsBuilder = optionsBuilder;
+        }
+
+        /// <summary>
+        ///     Configures the context to use the provided <see cref="IExecutionStrategy" />.
+        /// </summary>
+        /// <param name="getExecutionStrategy"> A function that returns a new instance of an execution strategy. </param>
+        public virtual MongoDbContextOptionsBuilder ExecutionStrategy(
+            [NotNull] Func<ExecutionStrategyDependencies, IExecutionStrategy> getExecutionStrategy)
+            => WithOption(e => e.WithExecutionStrategyFactory(Check.NotNull(getExecutionStrategy, nameof(getExecutionStrategy))));
+
+        /// <summary>
+        /// Configures the context to use the provided Region.
+        /// </summary>
+        /// <param name="region">MongoDb region name</param>
+        public virtual MongoDbContextOptionsBuilder Region([NotNull] string region)
+            => WithOption(e => e.WithRegion(Check.NotNull(region, nameof(region))));
+
+        /// <summary>
+        ///     Sets an option by cloning the extension used to store the settings. This ensures the builder
+        ///     does not modify options that are already in use elsewhere.
+        /// </summary>
+        /// <param name="setAction"> An action to set the option. </param>
+        /// <returns> The same builder instance so that multiple calls can be chained. </returns>
+        protected virtual MongoDbContextOptionsBuilder WithOption([NotNull] Func<MongoDbOptionsExtension, MongoDbOptionsExtension> setAction)
+        {
+            ((IDbContextOptionsBuilderInfrastructure)_optionsBuilder).AddOrUpdateExtension(
+                setAction(_optionsBuilder.Options.FindExtension<MongoDbOptionsExtension>() ?? new MongoDbOptionsExtension()));
+
+            return this;
+        }
+
+        #region Hidden System.Object members
+
+        /// <summary>
+        ///     Returns a string that represents the current object.
+        /// </summary>
+        /// <returns> A string that represents the current object. </returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override string ToString() => base.ToString();
+
+        /// <summary>
+        ///     Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj"> The object to compare with the current object. </param>
+        /// <returns> true if the specified object is equal to the current object; otherwise, false. </returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) => base.Equals(obj);
+
+        /// <summary>
+        ///     Serves as the default hash function.
+        /// </summary>
+        /// <returns> A hash code for the current object. </returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => base.GetHashCode();
+
+        #endregion
+    }
+}
